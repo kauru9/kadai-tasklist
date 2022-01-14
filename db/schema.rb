bece7ec95801f5924b33ee10d3d36063c4c8ed6e
natu <aya.natu0823@gmail.com>
2022-01-14 10:17:46 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_07_074840) do
+ActiveRecord::Schema.define(version: 2022_01_14_093635) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
