54e42265ae140edcf927d6809e9937ebf4d6c848
natu <aya.natu0823@gmail.com>
2022-02-04 05:08:31 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
       @tasks = Task.all
@@ -12,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
